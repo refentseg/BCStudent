0667a5d422a0bfe4e55503f70dcdc400cd7c6ed3
DineoKabini21 <576500@student.belgiumcampus.ac.za>
2022-11-13 23:00:54 +0200

    Not done

--- BCStudent_Refentse_Gaonnwe_Dineo_Kabini_Netshedzo_Mmbengwa/Register.Designer.cs
+++ BCStudent_Refentse_Gaonnwe_Dineo_Kabini_Netshedzo_Mmbengwa/Register.Designer.cs
@@ -1,6 +1,7 @@
-﻿namespace BCStudent_Refentse_Gaonnwe_Dineo_Kabini_Netshedzo_Mmbengwa
+﻿
+namespace BCStudent_Refentse_Gaonnwe_Dineo_Kabini_Netshedzo_Mmbengwa
 {
-    partial class Form1
+    partial class Register
     {
         /// <summary>
         /// Required designer variable.
@@ -28,27 +29,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
             this.lblUsername = new System.Windows.Forms.Label();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
+            this.lblConfirm = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.lblregister = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnLogin
+            // btnRegister
             // 
-            this.btnLogin.Location = new System.Drawing.Point(69, 296);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(100, 51);
-            this.btnLogin.TabIndex = 0;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnRegister.Location = new System.Drawing.Point(77, 326);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(113, 61);
+            this.btnRegister.TabIndex = 0;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(65, 88);
+            this.lblUsername.Location = new System.Drawing.Point(73, 105);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(87, 20);
             this.lblUsername.TabIndex = 1;
@@ -56,49 +60,77 @@
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(216, 88);
+            this.txtUsername.Location = new System.Drawing.Point(234, 105);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(219, 26);
+            this.txtUsername.Size = new System.Drawing.Size(229, 26);
             this.txtUsername.TabIndex = 2;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(65, 158);
+            this.lblPassword.Location = new System.Drawing.Point(73, 168);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(82, 20);
             this.lblPassword.TabIndex = 3;
             this.lblPassword.Text = "Password:";
             // 
+            // lblConfirm
+            // 
+            this.lblConfirm.AutoSize = true;
+            this.lblConfirm.Location = new System.Drawing.Point(73, 227);
+            this.lblConfirm.Name = "lblConfirm";
+            this.lblConfirm.Size = new System.Drawing.Size(141, 20);
+            this.lblConfirm.TabIndex = 4;
+            this.lblConfirm.Text = "Confirm Password:";
+            // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(216, 158);
+            this.txtPassword.Location = new System.Drawing.Point(234, 162);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(219, 26);
-            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Size = new System.Drawing.Size(229, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Location = new System.Drawing.Point(234, 221);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.Size = new System.Drawing.Size(229, 26);
+            this.txtConfirm.TabIndex = 6;
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(314, 296);
+            this.btnCancel.Location = new System.Drawing.Point(329, 326);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(100, 51);
-            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Size = new System.Drawing.Size(113, 61);
+            this.btnCancel.TabIndex = 7;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // lblregister
+            // 
+            this.lblregister.AutoSize = true;
+            this.lblregister.Location = new System.Drawing.Point(138, 23);
+            this.lblregister.Name = "lblregister";
+            this.lblregister.Size = new System.Drawing.Size(110, 20);
+            this.lblregister.TabIndex = 8;
+            this.lblregister.Text = "Register Form";
+            // 
+            // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblregister);
             this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtConfirm);
             this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblConfirm);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.btnLogin);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnRegister);
+            this.Name = "Register";
+            this.Text = "Register";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,12 +138,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblConfirm;
         private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirm;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblregister;
     }
 }
-
